3b293c6a06e29c16d1dd413097e9c411ebad6edc
AlejandroNicolasValenti <nicolaspiu12@gmail.com>
2023-12-11 20:23:16 -0300

    basededatos

---
+++ src/models/PostModel.js
@@ -0,0 +1,31 @@
+import { Schema, model } from "mongoose";
+
+const PostSchema = new Schema(
+  {
+    titulo: {
+      type: String,
+      required: true,
+    },
+    contenido: {
+      type: String,
+      required: true,
+    },
+    comentarios: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Comentario",
+      },
+    ],
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "Usuario",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
+
+export const PostModel = model("Post", PostSchema);
